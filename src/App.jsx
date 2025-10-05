4da38870c0f257308d3781ba8df56ec16e04ee47
Sergey Korsensky <sergey.korsensky@intel.com>
2025-10-05 15:51:19 +0300

    Implement saving the game history

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function Square({ value, onSquareClick }) {
 }
 
 
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true)
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
@@ -29,8 +27,7 @@ export default function Board() {
         } else {
             nextSquares[i] = "O";
         }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext)
+        onPlay(nextSquares);
     }
 
     return (
@@ -74,4 +71,27 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares){
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext)
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+        </div>
+        // <div className={"game-info"}>
+        //     <ol>{ /* TODO */}</ol>
+        // </div>
+    );
 }
